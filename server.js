f1f9f0773a5012bcc1bf0c87ac392d5afca1fd22
Chase Olsen <chase.olsen22@gmail.com>
2016-11-10 16:02:29 -0700

    Hot fixes made

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var middleware = require('./controllers/middleware.js');
 var mainCtrl = require('./controllers/mainCtrl.js');
+// var session = require('express-session');
+// var config = require(./config.json);
 
 var app = express();
 
@@ -9,8 +11,35 @@ app.use(bodyParser.json());
 // app.use(cors());
 app.use(middleware.addHeaders);
 
+// app.use(session({
+//   secret: config.sessionSecret,
+//   saveUninitialized: false,
+//   resave: false
+// }));
+
 app.use(middleware.addHeaders);
 // app.use(express.static('public')); <- name of folder (angular contents/projects in public folder)
+
+
+//____________________________________________________
+// app.get('/wishlist', function(req, res, next){
+//   res.json(req.session.wishlist);
+// });
+//
+// app.post('/wishlist', function(){
+//  console.log(req.session);
+//
+//    -----if no wishlist
+//  if(!req.session.wishlist){
+//   req.session.wishlist = [];
+//    }
+//
+//  req.session.wishlist.push(req.body);
+//  res.sendStatus(204);
+//
+// });
+//____________________________________________________
+
 
 app.get('/name', mainCtrl.getName);
 app.get('/location', mainCtrl.getLocation);
